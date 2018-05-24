39d4783940a9d726c179c0a3419f49803dcd2096
coldbear <kolesnal@hu-berlin.degit config --global user.name kolesnalgit config --global user.email kolesnal@hu-berlin.de>
2018-05-24 12:00:48 +0200

    div corrections

--- VCRIXindex/VCRIXindex.R
+++ VCRIXindex/VCRIXindex.R
@@ -130,39 +130,30 @@ index <- data.frame("vola" = sqrt(var), "date" = ts$date)
 index$vcrix = c(1:nrow(index))
 index$divisor <- c(1:nrow(index))
 index$day = lubridate::day(index$date)
+index$month = lubridate::month(index$date)
+
+
+#Set up the re-evaluation threshold date (every first day of Feb, May, August, November)
+index$recalc <- 0
+index$recalc<-ifelse((index$day==1)&(index$month==2|index$month== 5|index$month== 8|index$month== 11), 1, 0)
+index$day <- NULL
+index$month <- NULL
+
+#Setting the base value of VCRIX to 1000
+    
 index$vcrix[1] = 1000
-index$divisor[1] = index$vola[1] / index$vcrix[1]
-index$divisor[2:92] = index$divisor[1]
-index$vcrix[2:92] = index$vola[2:92] / index$divisor[2:92]
-index$vcrix[93] = index$vcrix[92]
+index$divisor[1] =index$vola[1]/index$vcrix[1]
 
-index$divisor[93:184] = index$vola[93] / index$vcrix[93]
-index$vcrix[94:184] = index$vola[94:184] / index$divisor[94:184]
 
-index$vcrix[185] = index$vcrix[184]
-index$divisor[185:276] = index$vola[185] / index$vcrix[185]
-index$vcrix[186:276] = index$vola[186:276] / index$divisor[186:276]
-
-index$vcrix[277] = index$vcrix[276]
-index$divisor[277:365] = index$vola[277] / index$vcrix[277]
-index$vcrix[277:365] = index$vola[277:365] / index$divisor[277:365]
-
-index$vcrix[366] = index$vcrix[365]
-index$divisor[366:457] = index$vola[366] / index$vcrix[366]
-index$vcrix[366:457] = index$vola[366:457] / index$divisor[366:457]
-
-index$vcrix[458] = index$vcrix[457]
-index$divisor[458:549] = index$vola[458] / index$vcrix[458]
-index$vcrix[458:549] = index$vola[458:549] / index$divisor[458:549]
-
-index$vcrix[550] = index$vcrix[549]
-index$divisor[550:641] = index$vola[550] / index$vcrix[550]
-index$vcrix[550:641] = index$vola[550:641] / index$divisor[550:641]
-
-index$vcrix[642] = index$vcrix[641]
-index$divisor[642:730] = index$vola[642] / index$vcrix[642]
-index$vcrix[642:730] = index$vola[642:730] / index$divisor[642:730]
-
+for (i in 2:nrow(index)) {
+  if (index$recalc[i] == 1) {
+    index$vcrix[i] =index$vcrix[i-1]
+    index$divisor[i] =index$vola[i]/index$vcrix[i]
+  } else {
+    index$divisor[i] =index$divisor[i-1]
+    index$vcrix[i] =index$vola[i]/index$divisor[i] 
+  }
+}
 
 #Plot VCRIX
 
